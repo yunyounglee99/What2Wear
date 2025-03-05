d83db78f9a15409221c83235a47a2bb7cd0e8e6a
nyoung <kembel0116@gmail.com>
2025-03-05 14:59:37 +0900

    add configs

---
+++ models/train.py
@@ -0,0 +1,10 @@
+import json
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import Dataset, DataLoader, random_split
+from datasets import Dataset, features
+from torch.optim import Adam
+from torch.optim.lr_scheduler import StepLR
+
+def train(json_path, save_path, num_epoch, batch_size):
